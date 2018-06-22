bcd222451a754b6c02505c0cf3888d3f2ed81608
Mariam Trubitsyn <mariam.trubitsyn@kronos.com>
2018-06-22 11:57:47 -0400

    try to create a conflict

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 AlphaNum=1
-""""put some comments
-for Alpha in (" Good night !!!") :
+""""put some comments12345
+"" put more comments
+for Alpha in (" Good morning !!!") :
 	print ("Letter ", AlphaNum, " is ", Alpha)
 	AlphaNum+=1
